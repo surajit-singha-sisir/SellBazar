4ea3f2674453888938d8ee03844eebf87e6426d1
Surajit Singha Sisir <sci.surajit@gmail.com>
2026-06-05 05:18:42 +0600

    Improved visual appearance and styling
    Changes include: Updated 2 stylesheets, Modified main.ts
    
    Modified 90 lines (+87 additions, -3 deletions)
    
    Signed-off-by: Surajit Singha Sisir <sci.surajit@gmail.com>

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
-import './assets/styles/main.scss'
+import './assets/styles/main.scss'   // variables + mixins + Tailwind + base styles
+import './assets/styles/_admin.scss' // admin component styles (after Tailwind preflight)
+import './assets/styles/_hero.scss'  // hero/landing styles (after Tailwind preflight)
 
 const app = createApp(App)
 app.use(createPinia())
